7ce99a9830d979265593c103291d0511c2b13c0d
Luna Herder <luna@herderfactory.nl>
2020-10-16 10:11:43 +0200

    Delete Button

---
+++ ViewModels/DeleteViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Repairshop.Models;
+
+namespace Repairshop.ViewModels
+{
+    public class DeleteViewModel
+    {
+        public RepairOrder order { get; set; }
+    }
+}
